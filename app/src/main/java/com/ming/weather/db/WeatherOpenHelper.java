263387f40afb2cabd7543ca20bc60ac88b7c1d2d
ming <3133309264@qq.com>
2019-05-12 19:03:44 +0800

    First commit.

---
+++ app/src/main/java/com/ming/weather/db/WeatherOpenHelper.java
@@ -0,0 +1,52 @@
+package com.ming.weather.db;
+
+import android.content.Context;
+import android.database.DatabaseErrorHandler;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import java.security.PublicKey;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+public class WeatherOpenHelper extends SQLiteOpenHelper {
+
+    //表：省
+    public static final String CREATE_PROVINCE = "create table Province ("
+        + "id integer primary key autoincrement, "
+        + "province_name text, "
+        + "province_code text)";
+
+    //表：市
+    public static final String CREATE_CITY = "create table City ("
+            + "id integer primary key autoincrement, "
+            + "city_name text, "
+            + "city_code text, "
+            + "province_id integer)";
+
+    //表：县
+    public static final String CREATE_COUNTY = "create table County ("
+            + "id integer primary key autoincrement, "
+            + "county_name text, "
+            + "county_code text, "
+            + "city_id integer)";
+
+
+    public WeatherOpenHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(CREATE_PROVINCE);
+        db.execSQL(CREATE_CITY);
+        db.execSQL(CREATE_COUNTY);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
